a543f94439c751e38e3bcb011e1882d9f305c04e
IslamHassan94 <islamhasan@hotmail.com>
2024-02-08 13:48:28 +0200

    Added comment

--- src/test/java/com/qacart/todo/base/BaseTest.java
+++ src/test/java/com/qacart/todo/base/BaseTest.java
@@ -38,6 +38,7 @@ public class BaseTest {
 
     @AfterMethod
     public void tearDown(ITestResult result) {
+        // Add extra code for test github actions trigger
         String testCaseName = result.getMethod().getMethodName();
         File destFile = new File("target" + File.separator + "screenshots" + File.separator + testCaseName + ".png");
         takeScreenshot(destFile);
